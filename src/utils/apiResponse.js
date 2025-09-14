b3873c2ffcd8f64b0a14aca0ff1604b9048561bc
Your Name <you@example.com>
2025-09-14 23:23:05 +0530

    add user and video models

--- src/utils/apiResponse.js
+++ src/utils/apiResponse.js
@@ -3,6 +3,6 @@ class apiResponse {
         this.statusCode = statusCode
         this.data = data
         this.message = message
-        this.succes = statusCode < 400
+        this.success = statusCode < 400
     }
 }
